fceb4d70b0136d5f8cbe45d61f0ec214bdce533c
Mouad-Aljabri <75577854+Mouad-Aljabri@users.noreply.github.com>
2023-05-11 21:41:25 +0300

    weapon inv

--- Assets/Scripts/GUI/InvWeaponDisplay.cs
+++ Assets/Scripts/GUI/InvWeaponDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using LlamAcademy.Guns;
+using LlamAcademy.Guns.Demo;
 using UnityEngine.UI;
 public class InvWeaponDisplay : MonoBehaviour
 {
@@ -10,15 +11,30 @@ public class InvWeaponDisplay : MonoBehaviour
     public TextMeshProUGUI weaponName;
     public TextMeshProUGUI ammo;
     public Image icon;
+    private int maxAmmo;
+    private PlayerGunSelector gunSelector;
+    private GunScriptableObject weaponClone;
+
+    void Awake()
+    {
+        gunSelector = GameObject.Find("Player").GetComponent<PlayerGunSelector>();
+    }
+
     void Start()
     {
         string path = "Guns/" + gameObject.name;
         weapon = Resources.Load<GunScriptableObject>(path);
         weaponName.text = weapon.name;
         icon.sprite = weapon.icon;
-        int currentAmmo = PlayerDataManager.currentProf.getWeapon(weapon.name).currentAmmo;
-        int maxAmmo = weapon.AmmoConfig.MaxAmmo;
-        ammo.text = currentAmmo.ToString()+"/"+maxAmmo.ToString();
+
+        weaponClone = gunSelector.instancedGuns.Find(weapon => weapon.Name==gameObject.name);
+
+        ammo.text = weaponClone.AmmoConfig.CurrentClipAmmo.ToString()+"/"+weaponClone.AmmoConfig.MaxAmmo.ToString();
+    }
+
+    void Update()
+    {
+        ammo.text = weaponClone.AmmoConfig.CurrentClipAmmo.ToString() + "/" + weaponClone.AmmoConfig.MaxAmmo.ToString();
     }
 
 }
--- Assets/Scripts/GUI/PlayerDataManager.cs
+++ Assets/Scripts/GUI/PlayerDataManager.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class PlayerDataManager
-{
-    public static PlayerData prof1;
-    public static PlayerData prof2;
-    public static PlayerData prof3;
-    public static PlayerData currentProf;
-}
+    public static class PlayerDataManager
+    {
+        public static PlayerData prof1;
+        public static PlayerData prof2;
+        public static PlayerData prof3;
+        public static PlayerData currentProf;
+    }
